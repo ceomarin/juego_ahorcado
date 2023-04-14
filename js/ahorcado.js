2ac9beb9b6e7f9680f9340387754d380ba046e5c
ceomarin <cmarin.ing@gmail.com>
2023-04-14 12:59:18 -0400

    modificando js

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -6,6 +6,13 @@ var letra5= document.getElementById("letra5");
 var letra6= document.getElementById("letra6");
 var letra7= document.getElementById("letra7");
 var casillaLetra = document.getElementById("letra");
+
+// casillaLetra.addEventListener("change",(e)=>{
+
+//     // ingresarLetra(casillaLetra.value);
+// })
+
+var btnJugar = document.querySelector("#jugar");
 // var = document.getElementById();
 // var = document.getElementById();
 // var = document.getElementById();
@@ -20,11 +27,17 @@ var listaPalabras= ["conocer","proceso","hermosa","mejorar","aspecto","momento",
 "generar","empezar","montaña","inferir","cliente","obligar","señalar","afirmar","asistir","detalle","existir",
 "plasmar","muestra","delgado","definir","exponer","obtener"];
 
-var letraIngresada ="";
+var letra ="";
 
 const letras = [];
 
 var palabraElegida = "";
+
+
+
+// eventos
+btnJugar.addEventListener("click",ingresarLetra);
+
 
 function letraEnPantalla(index){
     var letraCorrecta;
@@ -55,36 +68,51 @@ function letraEnPantalla(index){
             //Crushear la página
     }
     //reemplazar texto de la casilla ID="AdHoc"
-    letraCorrecta.innerText=letraIngresada.toUpperCase();
+    letraCorrecta.innerText=letra.toUpperCase();
 }
 
 function mostrarLetra(){
     for (i=0; i<palabraElegida.length;i++){
-        if(letraIngresada == palabraElegida[i]){
+        if(letra == palabraElegida[i]){
             //reemplazar espacio por la letra acertada
-            letraEnPantalla();
+            console.log(letra);
+            letraEnPantalla(i);
         }
     }
 }
 
 // al ingresar letra:
-function ingresarLetra(letra){
+function ingresarLetra(){
     letra = casillaLetra.value.toLowerCase(); /****************/
-    if (letra in letras){
-        alert("Ya ha ingresado ésta letra, porfavor intentelo nuevamente.")
+    console.log(letras);
+    if(letras.length === 0){
+        letras.push(letra);
+        mostrarLetra();
+    }else{
+        for(let i = 0; i < letras.length;i++ ){
+            if (letra === letras[i]){
+                alert("Ya ha ingresado ésta letra, porfavor intentelo nuevamente.");
+                break;
+
+            }
+            else{
+                letras.push(letra);
+                mostrarLetra();
+                break;
+            }
+        }
     }
-    else{
-        letras.push(letra)
-        mostrarLetra(letra)
-    }
+
+
+
 }
 
 //al cargar la página
 function elegirPalabra(){
-    //console.table(listaPalabras);
+    // console.table(listaPalabras);
     palabraElegida = listaPalabras[parseInt(Math.random()*listaPalabras.length-2)+1];        
     console.log(palabraElegida);
 }
 
-//console.log(listaPalabras.length);
+// console.log(listaPalabras.length);
 elegirPalabra();
